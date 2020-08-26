9ff06acdca33c87f111942f48437e67a44d657b0
Robert G. Schaffrath <robert@schaffrath.net>
2020-08-26 12:32:37 -0400

    All fixes and changes to support enhanced functionality.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var obj = {
   // issue #2 and going forward
-  Encoder: require('lib/same'),
-  Writer: require('lib/writer'),
-  Values: require('lib/fixtures/same')
+  Encoder: require('./lib/same'),
+  Writer: require('./lib/writer.js'),
+  Values: require('./lib/fixtures/same.js')
 };
 
 // back compat for all my no users :)
